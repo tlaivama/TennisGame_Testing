358be00a6ffa01004b32d4e5af34a95e0bdd9c80
tlaivama <tlaivama@MIKROTILAT1-79.student.yo.oulu.fi>
2017-09-20 15:03:27 +0300

    Assignment finished

--- tests/TennisGameTest.java
+++ tests/TennisGameTest.java
@@ -2,8 +2,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
-
 public class TennisGameTest {
 	
 // Here is the format of the scores: "player1Score - player2Score"
@@ -20,7 +18,7 @@ public class TennisGameTest {
 // "player2 has advantage"
 // "player1 wins"
 // "player2 wins"
-	@Ignore
+	@Test
 	public void testTennisGame_Start() {
 		//Arrange
 		TennisGame game = new TennisGame();
@@ -63,5 +61,128 @@ public class TennisGameTest {
 		//Act
 		// This statement should cause an exception
 		game.player1Scored();			
-	}		
+	}
+	
+	@Test
+	public void testTennisGame_CorrectOrderOfGetScoreReturnString() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		game.player1Scored();
+		
+		assertEquals("Score returned has the players in the wrong order", "15 - love", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_GetScoreReturnsCorrectTies() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		assertEquals("Incorrect score returned for 0 - 0", "love - love", game.getScore());
+		
+		bothPlayersScoreNTimes(game, 1);
+		assertEquals("Incorrect score returned for 1 - 1", "15 - 15", game.getScore());
+		
+		bothPlayersScoreNTimes(game, 1);
+		assertEquals("Incorrect score returned for 2 - 2", "30 - 30", game.getScore());
+		
+		bothPlayersScoreNTimes(game, 1);
+		assertEquals("Incorrect score returned for 3 - 3", "deuce", game.getScore());
+		
+		bothPlayersScoreNTimes(game, 3);
+		assertEquals("Incorrect score returned for 6 - 6", "deuce", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_Player1Advantage() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		bothPlayersScoreNTimes(game, 3);
+		game.player1Scored();
+		
+		assertEquals("Score is 4 - 3 but player1 does not have advantage", "player1 has advantage", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_Player2Advantage() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		bothPlayersScoreNTimes(game, 5);
+		game.player2Scored();
+		
+		assertEquals("Score is 5 - 6 but player2 does not have advantage", "player2 has advantage", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_Player1Points4_Player2Points0_Player1Wins() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		for (int i = 0; i < 4; ++i) {
+			game.player1Scored();
+
+		}
+		
+		assertEquals("Score is 4 - 0 but player1 has not won", "player1 wins", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_Player1Points0_Player2Points4_Player1Wins() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		for (int i = 0; i < 4; ++i) {
+			game.player2Scored();
+
+		}
+		
+		assertEquals("Score is 0 - 4 but player2 has not won", "player2 wins", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_Player1Points5_Player2Points3_Player1Wins() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		bothPlayersScoreNTimes(game, 3);
+		game.player1Scored();
+		game.player1Scored();
+		
+		assertEquals("Score is 5 - 3 but player1 has not won", "player1 wins", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_Player1Points4_Player2Points6_Player2Wins() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		bothPlayersScoreNTimes(game, 4);
+		game.player2Scored();
+		game.player2Scored();
+		
+		assertEquals("Score is 4 - 6 but player2 has not won", "player2 wins", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_30_love() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		for (int i = 0; i < 2; ++i) {
+			game.player1Scored();
+		}
+		
+		assertEquals("Score is 2 - 0 but getScore did not return 30 - love", "30 - love", game.getScore());
+	}
+	
+	@Test
+	public void testTennisGame_15_40() throws TennisGameException {
+		TennisGame game = new TennisGame();
+		
+		bothPlayersScoreNTimes(game, 1);
+		game.player2Scored();
+		game.player2Scored();
+		
+		assertEquals("Score is 2 - 0 but getScore did not return 15 - 40", "15 - 40", game.getScore());
+	}
+	
+	private void bothPlayersScoreNTimes(TennisGame game, int n) throws TennisGameException {
+		for (int i = 0; i < n; ++i) {
+			game.player1Scored();
+			game.player2Scored();
+		}
+	}
 }
